8ea3120da69e8eb9e32d7a358978020de562c813
davemarong <kmarong@hotmail.com>
2022-08-01 22:24:11 +0200

    use npm qr reader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
+
 function App() {
+  const [data, setData] = useState("No result");
+
   const openMediaDevices = async (constraints) => {
     return await navigator.mediaDevices.getUserMedia(constraints);
   };
@@ -9,7 +14,24 @@ function App() {
   } catch (error) {
     console.error("Error accessing media devices.", error);
   }
-  return <div>Hei du</div>;
+  return (
+    <div>
+      Hei du
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: "100%" }}
+      />
+      <p>{data}</p>
+    </div>
+  );
 }
 
 export default App;
