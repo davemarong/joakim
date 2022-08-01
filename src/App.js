28678604e2284cd2cd9964dc19ab1fc98cc629d0
davemarong <kmarong@hotmail.com>
2022-08-01 21:54:54 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const openMediaDevices = async (constraints) => {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  };
+
+  try {
+    const stream = openMediaDevices({ video: true, audio: true });
+    console.log("Got MediaStream:", stream);
+  } catch (error) {
+    console.error("Error accessing media devices.", error);
+  }
+  return <div>Hei du</div>;
 }
 
 export default App;
